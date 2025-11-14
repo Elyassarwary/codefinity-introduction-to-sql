9cce6b40af669c07fc4f260d00e46f1d82046459
Elyassarwary <>
2025-11-14 21:11:06 +0000

    AND Filtering

---
+++ advanced_data_filtering/and_filtering/query.sql
@@ -0,0 +1,4 @@
+-- Write a SQL query to select only the names of countries from Asia with a population greater than 1000000
+
+Select name From Country
+Where continent = 'Asia'and Population > '1000000'; 
